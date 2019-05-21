f4197646b3c03090d1d4640cb3a29efe177dfea3
Marina Kam <kam.marvic@gmail.com>
2019-05-21 19:23:20 +0300

    created build folder

---
+++ build/precache-manifest.0541e3ca9e3fcb0518a57839976b753c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba6f376a620d706208c99c6f0eb33e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b359726fc7ca77a16a",
+    "url": "/static/css/main.b784df01.chunk.css"
+  },
+  {
+    "revision": "f4242312f8bcd4cd70e0",
+    "url": "/static/js/2.0f4713c6.chunk.js"
+  },
+  {
+    "revision": "90b359726fc7ca77a16a",
+    "url": "/static/js/main.01cdfe14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "43b32b1f349b290d408ae00e183879e9",
+    "url": "/static/media/signed-out-home.43b32b1f.jpg"
+  },
+  {
+    "revision": "25398cf74efabfc8f446dfcb6c2a6338",
+    "url": "/static/media/warbler-logo.25398cf7.png"
+  }
+]);
