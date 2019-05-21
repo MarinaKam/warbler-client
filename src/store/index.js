060bbc71e046fa1a5692288d85d0aa144d7ab759
Marina Kam <kam.marvic@gmail.com>
2019-05-21 19:32:45 +0300

    changed set up of store

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,10 @@ import rootReducer from "./reducers";
 import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export function configureStore() {
     return createStore(
         rootReducer,
-        compose(
-            applyMiddleware(thunk),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
+        composeEnhancers(applyMiddleware(thunk))
     );
 }
